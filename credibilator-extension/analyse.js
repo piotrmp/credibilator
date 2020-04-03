391176b6f521b7df944c4bbcd6b0b73cb281fdda
Piotr <piotr.m.przybyla@gmail.com>
2020-04-03 11:53:41 +0200

    Added interpretable model.

--- credibilator-extension/analyse.js
+++ credibilator-extension/analyse.js
@@ -13,7 +13,6 @@ function unpackContainer(container){
 	document.getElementById("title").innerHTML=container.pageTitle;
 	document.getElementById("stylometricScore").innerHTML=container.stylometricScore
 	let counter=0
-	//document.getElementById("features").innerHTML='';
 	for (let feature in container.stylometricFeatures){
 		//document.getElementById("features").innerHTML+=(feature+": "+container.stylometricFeatures[feature]+"<br/>\n")
 		counter++;
@@ -44,7 +43,7 @@ async function loadModel(url,allcodes,allmasks){
 
 async function loadModelClick(){
 	document.getElementById("model").innerHTML="Loading..."
-	globalModel=await loadModel('./bilstmavg/data/tfjs-small/model.json');
+	globalModel=await loadModel('./bilstmavg/data/tfjs-small-interp/model.json');
 	document.getElementById("model").innerHTML="Loaded."
 	document.getElementById("processbutton").disabled=false;
 }
@@ -55,7 +54,7 @@ async function processClick(){
 	let content=globalContainer.textContent;
 	globalTokenised=tokenise_bilstmavg(content);
 	document.getElementById("scored").innerHTML ="Scoring...";
-	readWordCodes("./bilstmavg/data/tfjs-small/words.txt",handleWordCodes)
+	readWordCodes("./bilstmavg/data/tfjs-small-interp/words.txt",handleWordCodes)
 }
 
 // generate input vectors
@@ -105,23 +104,64 @@ function inputVectors(tokenised,wordCodes){
 // use the model it
 async function useModel(model,allcodes,allmasks){
 	try{
-		const prediction = model.predict([tf.tensor([allcodes]),tf.tensor([allmasks])]).dataSync();
-		return(prediction)
+		const prediction = model.predict([tf.tensor([allcodes]),tf.tensor([allmasks])]);
+		//let result=prediction.dataSync(); 
+		let result=[prediction[0].dataSync(), prediction[1].dataSync(), prediction[2].dataSync()];
+		return(result)
 	} catch (e) {
 		console.log(e);
 	}
 }
 
-// do the asynchronous processing
+// just for demo, to be replaced by super-fancy visualisation
+function showInterpretableNeural(tokenised,wordCodes,prediction){
+	console.log(prediction);
+	let sentenceScores=Array.from(prediction[1]);
+	console.log(sentenceScores);
+	let sentenceCoords=Array.from(prediction[2]);
+	let resultHTML="";
+	let i=0;
+	for (let sentence of tokenised){
+		let j=0
+		resultHTML+="<b>";
+		for (let token of sentence){
+			let word='[UNK]'
+			if (token in wordCodes){
+				word=token
+			}
+			resultHTML=resultHTML+word+" ";
+			j=j+1;
+			if (j==MAX_SEQUENCE_LENGTH){
+				break;
+			}
+		}
+		resultHTML+="</b><br/>\n";
+		resultHTML+="F: "+sentenceScores[i*2+1].toFixed(2)+" R: "+sentenceScores[i*2+0].toFixed(2);
+		resultHTML+="<br/>\n";
+		for (let k=0;k<200;++k){
+			resultHTML+=sentenceCoords[i*200+k].toFixed(2)+" ";
+		}
+		resultHTML+="<br/>\n";
+		i++;
+		if (i==MAX_DOCUMENT_LENGTH){
+			break;
+		}
+	}
+	document.getElementById("interpretation").innerHTML = resultHTML
+}
+
+// do the asynchronous processing and display the results
 async function handleWordCodes(wordCodes){
 	let inputVectors1=inputVectors(globalTokenised,wordCodes)
 	let allcodes=inputVectors1[0]
 	let allmasks=inputVectors1[1]
 	let resultHTML=inputVectors1[2]
 	let prediction=await useModel(globalModel,allcodes,allmasks);
-	let predR=Array.from(prediction)[0]
-	let predF=Array.from(prediction)[1]
+	let overallScore=Array.from(prediction[0]);
+	let predR=overallScore[0];
+	let predF=overallScore[1];
 	document.getElementById("scored").innerHTML = "NONCREDIBLE: "+predF+"<br/>CREDIBLE: "+predR;
+	showInterpretableNeural(globalTokenised,wordCodes,prediction);
 }
 
 
